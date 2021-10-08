e8337dc54f4ab4295116a40e09b094ab2845e03c
Eduardo <eduardo.gonzalez.flores28@gmail.com>
2021-10-08 16:22:29 -0500

    Agregando el proyecto

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/> My Personal Portafolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
