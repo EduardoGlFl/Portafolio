3cb33ec641bc7cd67cd947b0fbe2ea87f0fadd53
Eduardo <eduardo.gonzalez.flores28@gmail.com>
2022-02-23 16:57:48 -0500

    eliminate clicky

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -39,24 +39,6 @@ export default class MyDocument extends Document {
           <title>Eduardo Flores</title>
         </Head>
         <body>
-          <a title="Real Time Web Analytics" href="http://clicky.com/101355158">
-            <img
-              alt="Clicky"
-              src="//static.getclicky.com/media/links/badge.gif"
-              border="0"
-            />
-          </a>
-          <script async src="//static.getclicky.com/101355158.js"></script>
-          <noscript>
-            <p>
-              <img
-                alt="Clicky"
-                width="1"
-                height="1"
-                src="//in.getclicky.com/101355158ns.gif"
-              />
-            </p>
-          </noscript>
           <Main />
           <NextScript />
         </body>
